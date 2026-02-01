e8bb7f7d726a7b18d8fe7c7e07124d40d89368e4
Romanovskyi2137 <Romanovskyi2137@gmail.com>
2026-02-01 11:01:40 +0000

    chore: initial commit + some backend

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,22 @@
-console.log("hello world!")
+import express from 'express';
+import cors from 'cors';
+import { prisma } from '../lib/prisma';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/boards', async (req, res) => {
+  try {
+    const boards = await prisma.board.findMany();
+    res.json(boards);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch boards', e: error });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+});
