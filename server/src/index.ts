0bdfe95d682543d5999df889b23f746144f10591
Romanovskyi2137 <Romanovskyi2137@gmail.com>
2026-02-01 13:46:21 +0000

    chore: initial commit, project setup + some backend

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,22 +1,23 @@
-import express from 'express';
-import cors from 'cors';
-import { prisma } from '../lib/prisma';
+import cors from 'cors'
+import express from 'express'
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+import { prisma } from '../lib/prisma'
 
-app.use(cors());
-app.use(express.json());
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.use(cors())
+app.use(express.json())
 
 app.get('/api/boards', async (req, res) => {
-  try {
-    const boards = await prisma.board.findMany();
-    res.json(boards);
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to fetch boards', e: error });
-  }
-});
+	try {
+		const boards = await prisma.board.findMany()
+		res.json(boards)
+	} catch (error) {
+		res.status(500).json({ error: 'Failed to fetch boards', e: error })
+	}
+})
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server is running on http://localhost:${PORT}`);
-});
+	console.log(`🚀 Server is running on http://localhost:${PORT}`)
+})
