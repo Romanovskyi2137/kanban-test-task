383cb12a56ec1ecbd101a02f86cfc63301f72da1
Romanovskyi2137 <Romanovskyi2137@gmail.com>
2026-02-02 19:54:08 +0000

    feat(server): add columns api + some tests

---
+++ server/src/api/controllers/column.controller.ts
@@ -0,0 +1,52 @@
+import type { Request, Response } from 'express'
+
+import * as columnService from '@/src/api/controllers/serviсes/column.service'
+
+export const createColumn = async (req: Request, res: Response) => {
+	try {
+		const { title, boardId } = req.body
+		if (!title || !boardId) {
+			return res.status(400).json({ message: 'Title and boardId are required' })
+		}
+		const column = await columnService.createColumn(title, boardId)
+		res.status(201).json(column)
+	} catch (error) {
+		res.status(500).json({ message: 'Error creating column', error })
+	}
+}
+
+export const updateColumn = async (
+	req: Request<{ id: string }>,
+	res: Response
+) => {
+	try {
+		const id = parseInt(req.params.id)
+		const { title } = req.body
+
+		if (isNaN(id)) {
+			return res.status(400).json({ message: 'Invalid column ID' })
+		}
+
+		const updated = await columnService.updateColumn(id, title)
+		res.json(updated)
+	} catch (error) {
+		res.status(500).json({ message: 'Error updating column', error })
+	}
+}
+
+export const deleteColumn = async (
+	req: Request<{ id: string }>,
+	res: Response
+) => {
+	try {
+		const id = parseInt(req.params.id)
+		if (isNaN(id)) {
+			return res.status(400).json({ message: 'Invalid column ID' })
+		}
+
+		await columnService.deleteColumn(id)
+		res.status(204).send()
+	} catch (error) {
+		res.status(500).json({ message: 'Error deleting column', error })
+	}
+}
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import cors from 'cors'
 import express from 'express'
 
 import boardRouter from '@/src/api/routers/board.routes'
+import columnRouter from '@/src/api/routers/column.routes'
 
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 3000
 app.use(cors())
 app.use(express.json())
 app.use('/api/boards', boardRouter)
+app.use('/api/columns', columnRouter)
 
 app.listen(PORT, () => {
 	console.log(`🚀 Server is running on http://localhost:${PORT}`)
